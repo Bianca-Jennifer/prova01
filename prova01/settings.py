a14acc436d879526d2e8db7f654c267e5f2e8c3b
Bianca-Jennifer <bianca.kalyse@gmail.com>
2022-08-24 17:12:36 -0300

    parte 07

--- prova01/settings.py
+++ prova01/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'prova01.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
